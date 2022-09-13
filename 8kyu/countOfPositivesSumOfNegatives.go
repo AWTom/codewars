57884d32a53ffa3eeaf22049edccdb0dc61278a1
William Strealy <williamstrealy@gmail.com>
2022-09-13 17:52:23 -0600

    Add url to countOfPositivesSumOfNegatives.go

--- 8kyu/countOfPositivesSumOfNegatives.go
+++ 8kyu/countOfPositivesSumOfNegatives.go
@@ -6,6 +6,8 @@ Return an array, where the first element is the count of positives numbers and t
 If the input is an empty array or is null, return an empty array.
 */
 
+// https://www.codewars.com/kata/reviews/61155e7fc1b0460001494db6/groups/631fea34d663cf0001afbd32
+
 package kata
 
 func CountPositivesSumNegatives(numbers []int) []int {

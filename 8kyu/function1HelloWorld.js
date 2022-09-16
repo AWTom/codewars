1b00c0d31df3f088bb16c618ea8383595eee1a81
William Strealy <williamstrealy@gmail.com>
2022-09-16 00:07:24 -0500

    Add description, description url, and solution url to function1HelloWorld.js

--- 8kyu/function1HelloWorld.js
+++ 8kyu/function1HelloWorld.js
@@ -1,3 +1,14 @@
+/*
+Description:
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+*/
+
+// https://www.codewars.com/kata/523b4ff7adca849afe000035/
+// https://www.codewars.com/kata/reviews/523b4ff7adca849afe000038/groups/62b1a4bdf1f57400015f6baa
+
 function greet () {
   return String.fromCharCode(104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33)
 }

1965b9d481f7a1477d5fc0c2007723607dcdfbd3
William Strealy <williamstrealy@gmail.com>
2022-10-01 13:43:25 -0600

    Add multiplesOfThreeOrFive.js

---
+++ 6kyu/multiplesOfThreeOrFive.js
@@ -0,0 +1,16 @@
+/*
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+Courtesy of projecteuler.net (Problem 1)
+
+*/
+
+// https://www.codewars.com/kata/514b92a657cdc65150000006/
+// https://www.codewars.com/kata/reviews/516f30247c907a79f200033c/groups/6338959e49f0480001224b33/
+
+const solution = n => Array(n > 1 ? n : 1).fill(0).reduce((p, c, i) => p + i * (i % 3 < 1 || i % 5 < 1));

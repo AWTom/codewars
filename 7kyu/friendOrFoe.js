ad38307ccbb3d3d9d865ff7b2be2f6fc1ace3380
William Strealy <williamstrealy@gmail.com>
2022-10-06 18:13:06 -0500

    Add 7kyu/friendOrFoe.js

---
+++ 7kyu/friendOrFoe.js
@@ -0,0 +1,19 @@
+/*
+
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output.
+
+*/
+
+// https://www.codewars.com/kata/55b42574ff091733d900002f/
+// https://www.codewars.com/kata/reviews/565f42480b9d78fa10000063/groups/583d9af7f30065a737000134/
+
+const friend = friends => friends.filter(e => e.length === 4);

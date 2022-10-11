c894ce4833c2b8b1381e556e5e5c91c43ccde069
William Strealy <williamstrealy@gmail.com>
2022-10-11 00:06:38 -0500

    Add 8kyu/convertANumberToAString.js

---
+++ 8kyu/convertANumberToAString.js
@@ -0,0 +1,12 @@
+/*
+
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+*/
+
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/
+// https://www.codewars.com/kata/reviews/5265326f5fda8eb1160004cb/groups/56169ba56bcb4db79c00000e/
+
+const numberToString = num => num.toString();
